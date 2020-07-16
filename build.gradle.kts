7e25eacc9c41896768479944b11ccae3b34f74eb
Anuraag Agrawal <anuraaga@gmail.com>
2020-07-16 15:41:28 +0900

    Fix formatting of build file for consistency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.hypertrace.gradle.publishing.HypertracePublishExtension
 import org.hypertrace.gradle.publishing.License
 
 plugins {
-  id ("org.hypertrace.repository-plugin") version "0.1.2"
+  id("org.hypertrace.repository-plugin") version "0.1.2"
   id("org.hypertrace.ci-utils-plugin") version "0.1.1"
   id("org.hypertrace.publish-plugin") version "0.3.0" apply false
   id("org.hypertrace.jacoco-report-plugin") version "0.1.0" apply false
