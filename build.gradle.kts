d4b59e6bf137311c03e947c9454da10da255e13a
Adrian Cole <adrian.f.cole@gmail.com>
2020-08-17 16:35:12 +0800

    Locks Java to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,4 +15,11 @@ subprojects {
       license.set(License.APACHE_2_0)
     }
   }
+
+  pluginManager.withPlugin("java") {
+    configure<JavaPluginExtension> {
+      sourceCompatibility = JavaVersion.VERSION_11
+      targetCompatibility = JavaVersion.VERSION_11
+    }
+  }
 }
