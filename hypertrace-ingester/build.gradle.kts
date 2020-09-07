aa6764ca4f5e9a199cd3d004bd9e3f8704254743
kotharironak <53209990+kotharironak@users.noreply.github.com>
2020-09-07 15:14:36 +0530

    chore: adds gradle, snyk and circle steps (#4)
    * chore: adds gradle, snyk and circle steps
    
    * update with apache license
    
    * Update .circleci/config.yaml
    
    Co-authored-by: ravisingal <62086374+ravisingal@users.noreply.github.com>
    
    * addressed Ravi's comments
    
    * moved back to community license
    
    * updating gradle to latest 6.6.1
    
    * Update .circleci/config.yaml
    
    Co-authored-by: ravisingal <62086374+ravisingal@users.noreply.github.com>

---
+++ hypertrace-ingester/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+  java
+  application
+  jacoco
+  id("org.hypertrace.docker-java-application-plugin")
+  id("org.hypertrace.docker-publish-plugin")
+  id("org.hypertrace.jacoco-report-plugin")
+}
+
+
+dependencies {
+}
