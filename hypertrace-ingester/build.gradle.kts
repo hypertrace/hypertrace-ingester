474d98e7e3e09454e6bd2fd05eb727bbe05e9fe0
Ronak <ronak@traceable.ai>
2020-09-11 19:47:03 +0530

    adding initial changes

--- hypertrace-ingester/build.gradle.kts
+++ hypertrace-ingester/build.gradle.kts
@@ -7,6 +7,50 @@ plugins {
   id("org.hypertrace.jacoco-report-plugin")
 }
 
+application {
+  mainClassName = "org.hypertrace.core.serviceframework.PlatformServiceLauncher"
+}
+
+hypertraceDocker {
+  defaultImage {
+    javaApplication {
+      serviceName.set("${project.name}")
+      adminPort.set(9099)
+    }
+  }
+}
 
 dependencies {
+  implementation("org.hypertrace.core.kafkastreams.framework:kafka-streams-framework:0.1.2")
+  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.9")
+  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.8")
+  implementation("org.hypertrace.core.datamodel:data-model:0.1.7")
+
+  implementation("org.hypertrace.core.spannormalizer:span-normalizer")
+  implementation("org.hypertrace.core.rawspansgrouper:raw-spans-grouper")
+}
+
+// Config for gw run to be able to run this locally. Just execute gw run here on Intellij or on the console.
+tasks.run<JavaExec> {
+  jvmArgs = listOf("-Dbootstrap.config.uri=file:${project.buildDir}/resources/main/configs", "-Dservice.name=${project.name}")
+}
+
+tasks.processResources {
+  dependsOn("copyServiceConfigs");
+}
+
+tasks.register<Copy>("copyServiceConfigs") {
+  with(
+      createCopySpec("span-normalizer", "span-normalizer"),
+      createCopySpec("raw-spans-grouper", "raw-spans-grouper")
+  ).into("./build/resources/main/configs/")
+}
+
+fun createCopySpec(projectName: String, serviceName: String): CopySpec {
+  return copySpec {
+    from("../${projectName}/${serviceName}/src/main/resources/configs/common") {
+      include("application.conf")
+      into("${serviceName}")
+    }
+  }
 }
--- hypertrace-ingester/src/main/java/org/hypertrace/ingester/HypertraceIngester.java
+++ hypertrace-ingester/src/main/java/org/hypertrace/ingester/HypertraceIngester.java
@@ -1,5 +1,84 @@
 package org.hypertrace.ingester;
 
-public class HypertraceIngester {
+import com.typesafe.config.Config;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+import org.hypertrace.core.kafkastreams.framework.KafkaStreamsApp;
+import org.hypertrace.core.rawspansgrouper.RawSpansGrouper;
+import org.hypertrace.core.serviceframework.config.ConfigClient;
+import org.hypertrace.core.serviceframework.config.ConfigClientFactory;
+import org.hypertrace.core.serviceframework.config.ConfigUtils;
+import org.hypertrace.core.spannormalizer.SpanNormalizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Hypertrace ingestion pipeline
+ * */
+public class HypertraceIngester extends KafkaStreamsApp {
+
+  private static final Logger logger = LoggerFactory.getLogger(HypertraceIngester.class);
+
+  private static final String CLUSTER_NAME = "cluster.name";
+  private static final String POD_NAME = "pod.name";
+  private static final String CONTAINER_NAME = "container.name";
+  private static final String KAFKA_STREAMS_CONFIG_KEY = "kafka.streams.config";
+
+  public HypertraceIngester(ConfigClient configClient) {
+    super(configClient);
+  }
+
+  @Override
+  public StreamsBuilder buildTopology(Map<String, Object> streamsProperties,
+      StreamsBuilder streamsBuilder,
+      Map<String, KStream<?, ?>> inputStreams) {
+
+    // create span normalizer
+    SpanNormalizer spanNormalizer = new SpanNormalizer(ConfigClientFactory.getClient());
+    Config spanNormalizerConfig = getJobConfig("span-normalizer");
+    Map<String, Object> spanNormalizerConfigMap = spanNormalizer.getStreamsConfig(spanNormalizerConfig);
+    streamsBuilder = spanNormalizer.buildTopology(spanNormalizerConfigMap, streamsBuilder, inputStreams);
+
+    // create raw spans grouper
+    RawSpansGrouper rawSpansGrouper = new RawSpansGrouper(ConfigClientFactory.getClient());
+    Config rawSpansGrouperConfig = getJobConfig("raw-spans-grouper");
+    Map<String, Object> rawSpansGrouperConfigMap = rawSpansGrouper.getStreamsConfig(rawSpansGrouperConfig);
+    streamsBuilder = rawSpansGrouper.buildTopology(rawSpansGrouperConfigMap, streamsBuilder, inputStreams);
+
+    return streamsBuilder;
+  }
+
+  @Override
+  public Map<String, Object> getStreamsConfig(Config jobConfig) {
+    Map<String, Object> streamsConfig = new HashMap<>(
+        ConfigUtils.getFlatMapConfig(jobConfig, KAFKA_STREAMS_CONFIG_KEY));
+    return streamsConfig;
+  }
+
+  @Override
+  public Logger getLogger() {
+    return logger;
+  }
+
+  @Override
+  public List<String> getInputTopics() {
+    return new ArrayList<>();
+  }
+
+  @Override
+  public List<String> getOutputTopics() {
+    return new ArrayList<>();
+  }
+
+  private Config getJobConfig(String jobName) {
+    return configClient.getConfig(jobName,
+        ConfigUtils.getEnvironmentProperty(CLUSTER_NAME),
+        ConfigUtils.getEnvironmentProperty(POD_NAME),
+        ConfigUtils.getEnvironmentProperty(CONTAINER_NAME)
+    );
+  }
 }
