db5ce55141b5374068dee40224f8d660a5369faa
surajpuvvada <45047893+surajpuvvada@users.noreply.github.com>
2020-09-25 01:13:13 -0700

    Use kafka-streams-framework  (#20)
    * Use kafka-streams-framework +
    Check if KStream for an input topic already exists and re-use it.
    
    * Updates to config
    
    * updates the configs for pre_create.topics option
    
    * updates to latest version of view-gen framework
    
    * Update gradle.properties
    
    * cleaned up the PRs with final changes
    
    * Update gradle.properties
    
    * reverted back to master copy as there was no change
    
    Co-authored-by: Ronak <ronak@traceable.ai>
    Co-authored-by: kotharironak <53209990+kotharironak@users.noreply.github.com>

--- hypertrace-view-creator/build.gradle.kts
+++ hypertrace-view-creator/build.gradle.kts
@@ -17,7 +17,7 @@ tasks.test {
 
 dependencies {
   implementation(project(":hypertrace-view-generator-api"))
-  implementation("org.hypertrace.core.viewcreator:view-creator-framework:0.1.11")
+  implementation("org.hypertrace.core.viewcreator:view-creator-framework:0.1.14")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
   testImplementation("org.mockito:mockito-core:3.3.3")
--- hypertrace-view-generator/build.gradle.kts
+++ hypertrace-view-generator/build.gradle.kts
@@ -26,7 +26,8 @@ tasks.test {
 
 dependencies {
   implementation(project(":hypertrace-view-generator-api"))
-  implementation("org.hypertrace.core.viewgenerator:view-generator-framework:0.1.11")
+
+  implementation("org.hypertrace.core.viewgenerator:view-generator-framework:0.1.14")
   implementation("org.hypertrace.core.datamodel:data-model:0.1.8")
   implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.9")
 
