49e1887cc4bd8215ee663c1224fa6369f15ce510
mohit-a21 <56842175+mohit-a21@users.noreply.github.com>
2020-10-08 23:28:33 +0530

    Updated data models to latest (#29)

--- raw-spans-grouper/build.gradle.kts
+++ raw-spans-grouper/build.gradle.kts
@@ -35,7 +35,7 @@ tasks.test {
 
 dependencies {
     implementation("org.hypertrace.core.spannormalizer:span-normalizer-api:0.1.23")
-    implementation("org.hypertrace.core.datamodel:data-model:0.1.8")
+    implementation("org.hypertrace.core.datamodel:data-model:0.1.9")
     implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.9")
     implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.9")
 
