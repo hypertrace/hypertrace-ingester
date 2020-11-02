676bfc417c3d12c0d7184274955f267374ea6624
Adrian Cole <adrian.f.cole@gmail.com>
2020-11-02 11:02:06 +0800

    removes redundant publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
   id("org.hypertrace.repository-plugin") version "0.2.1"
   id("org.hypertrace.ci-utils-plugin") version "0.1.2"
-  id("org.hypertrace.publish-plugin") version "0.3.0" apply false
   id("org.hypertrace.docker-java-application-plugin") version "0.7.1" apply false
   id("org.hypertrace.docker-publish-plugin") version "0.7.1" apply false
   id("org.hypertrace.jacoco-report-plugin") version "0.1.1" apply false
