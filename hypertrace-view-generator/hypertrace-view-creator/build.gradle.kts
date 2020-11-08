707b234272ccd3d99950d76b456389b0db3df6d2
Ronak <ronak@traceable.ai>
2020-11-08 15:15:32 +0530

    adds contrains on guava library suggested by snyk

--- hypertrace-view-generator/hypertrace-view-creator/build.gradle.kts
+++ hypertrace-view-generator/hypertrace-view-creator/build.gradle.kts
@@ -18,6 +18,11 @@ tasks.test {
 dependencies {
   implementation(project(":hypertrace-view-generator:hypertrace-view-generator-api"))
   implementation("org.hypertrace.core.viewcreator:view-creator-framework:0.1.16")
+  constraints {
+    implementation("com.google.guava:guava:30.0-jre") {
+      because("https://snyk.io/vuln/SNYK-JAVA-COMGOOGLEGUAVA-1015415")
+    }
+  }
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
   testImplementation("org.mockito:mockito-core:3.3.3")
