afc58286d3b5fe58698aad38c96c9f8acabab53a
kotharironak <53209990+kotharironak@users.noreply.github.com>
2020-11-09 15:37:40 +0530

    Updates the license file of span-normalizer and raw-spans-grouper (#24)

--- raw-spans-grouper/build.gradle.kts
+++ raw-spans-grouper/build.gradle.kts
@@ -1,3 +1,9 @@
 subprojects {
     group = "org.hypertrace.core.rawspansgrouper"
+
+    pluginManager.withPlugin("org.hypertrace.publish-plugin") {
+        configure<org.hypertrace.gradle.publishing.HypertracePublishExtension> {
+            license.set(org.hypertrace.gradle.publishing.License.APACHE_2_0)
+        }
+    }
 }
