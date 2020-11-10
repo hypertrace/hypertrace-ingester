9728c6458ed83d199486bf5464cc592e5970fe45
anujgoyal <anuj@traceable.ai>
2020-11-10 14:43:05 +0530

    fix dep name

--- hypertrace-view-generator/hypertrace-view-creator/build.gradle.kts
+++ hypertrace-view-generator/hypertrace-view-creator/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation("com.google.guava:guava:30.0-jre") {
       because("https://snyk.io/vuln/SNYK-JAVA-COMGOOGLEGUAVA-1015415")
     }
-    implementation("org.apache.calcite:calcite-core@1.26.0") {
+    implementation("org.apache.calcite:calcite-core:1.26.0") {
       because("https://snyk.io/vuln/SNYK-JAVA-ORGAPACHECALCITE-1038296")
     }
   }
