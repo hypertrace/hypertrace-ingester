498fdf86eb97c6b869a34281dbad779e8ffda281
GurtejSohi <gurtejsohi25@gmail.com>
2020-11-13 00:29:08 +0530

    Use first class Http fields directly instead of extracting fields out of URL (#39)

--- hypertrace-trace-enricher/hypertrace-trace-enricher-impl/src/main/java/org/hypertrace/traceenricher/enrichment/enrichers/resolver/backend/HttpBackendResolver.java
+++ hypertrace-trace-enricher/hypertrace-trace-enricher-impl/src/main/java/org/hypertrace/traceenricher/enrichment/enrichers/resolver/backend/HttpBackendResolver.java
@@ -3,11 +3,10 @@ package org.hypertrace.traceenricher.enrichment.enrichers.resolver.backend;
 import static org.hypertrace.traceenricher.util.EnricherUtil.createAttributeValue;
 import static org.hypertrace.traceenricher.util.EnricherUtil.setAttributeIfExist;
 
-import java.net.URI;
 import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 import org.hypertrace.core.datamodel.Event;
-import org.hypertrace.core.datamodel.shared.SpanAttributeUtils;
+import org.hypertrace.core.datamodel.eventfields.http.Request;
 import org.hypertrace.core.datamodel.shared.StructuredTraceGraph;
 import org.hypertrace.core.span.constants.RawSpanConstants;
 import org.hypertrace.core.span.constants.v1.Http;
@@ -25,12 +24,6 @@ import org.slf4j.LoggerFactory;
 public class HttpBackendResolver extends AbstractBackendResolver {
   private static final Logger LOGGER = LoggerFactory.getLogger(HttpBackendResolver.class);
 
-  private static final String HTTP_URL_ATTR = RawSpanConstants.getValue(Http.HTTP_URL_WITH_HTTP);
-  private static final String HTTP_HOST_ATTR = RawSpanConstants.getValue(Http.HTTP_HOST);
-  private static final String HTTP_PATH_ATTR = RawSpanConstants.getValue(Http.HTTP_PATH);
-  private static final String HTTP_REQUEST_URL_ATTR =
-      RawSpanConstants.getValue(Http.HTTP_REQUEST_URL);
-
   public HttpBackendResolver(FQNResolver fqnResolver) {
     super(fqnResolver);
   }
@@ -40,41 +33,19 @@ public class HttpBackendResolver extends AbstractBackendResolver {
     Protocol protocol = EnrichedSpanUtils.getProtocol(event);
 
     if (protocol == Protocol.PROTOCOL_HTTP || protocol == Protocol.PROTOCOL_HTTPS) {
-      String backendUriStr = null;
-      String path = null;
-      if (SpanAttributeUtils.containsAttributeKey(event, HTTP_URL_ATTR)) {
-        backendUriStr = SpanAttributeUtils.getStringAttribute(event, HTTP_URL_ATTR);
-      } else if (SpanAttributeUtils.containsAttributeKey(event, HTTP_REQUEST_URL_ATTR)) {
-        backendUriStr = SpanAttributeUtils.getStringAttribute(event, HTTP_REQUEST_URL_ATTR);
-      }
+      Optional<Request> httpRequest = Optional.ofNullable(event.getHttp())
+              .map(org.hypertrace.core.datamodel.eventfields.http.Http::getRequest);
+      String backend = httpRequest.map(Request::getHost).orElse(null);
+      String path = httpRequest.map(Request::getPath).orElse(null);
 
-      if (StringUtils.isNotEmpty(backendUriStr)) {
-        URI backendURI;
-        try {
-          backendURI = URI.create(backendUriStr);
-          backendUriStr = String.format("%s:%d", backendURI.getHost(), backendURI.getPort());
-          path = backendURI.getPath();
-        } catch (IllegalArgumentException e) {
-          LOGGER.warn("Failed to parse URL string {} to create URI", backendUriStr);
-          backendUriStr = null;
-        }
-      }
-      // if URL string was null or unable to parse URL string, check host attribute
-      if (StringUtils.isEmpty(backendUriStr) && SpanAttributeUtils.containsAttributeKey(event, HTTP_HOST_ATTR)) {
-        backendUriStr = SpanAttributeUtils.getStringAttribute(event, HTTP_HOST_ATTR);
-        if (SpanAttributeUtils.containsAttributeKey(event, HTTP_PATH_ATTR)) {
-          path = SpanAttributeUtils.getStringAttribute(event, HTTP_PATH_ATTR);
-        }
-      }
-
-      if (StringUtils.isEmpty(backendUriStr)) {
+      if (StringUtils.isEmpty(backend)) {
         // Shouldn't reach here.
         LOGGER.warn("Unable to infer a http backend from event: {}", event);
         return Optional.empty();
       }
 
       BackendType type = (protocol == Protocol.PROTOCOL_HTTPS) ? BackendType.HTTPS : BackendType.HTTP;
-      final Builder entityBuilder = getBackendEntityBuilder(type, backendUriStr, event);
+      final Builder entityBuilder = getBackendEntityBuilder(type, backend, event);
       if (StringUtils.isNotEmpty(path)) {
         entityBuilder.putAttributes(
             EntityConstants.getValue(BackendAttribute.BACKEND_ATTRIBUTE_PATH),
