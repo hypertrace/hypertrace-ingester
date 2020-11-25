3ba87670a86167954d90c098b1e045b9e379ef7e
Aaron Steinfeld <aaron@traceable.ai>
2020-11-25 08:07:53 -0500

    refactor: log and empty for unknown attribute value types

--- hypertrace-trace-enricher/trace-reader/src/main/java/org/hypertrace/trace/reader/entities/AttributeValueConverter.java
+++ hypertrace-trace-enricher/trace-reader/src/main/java/org/hypertrace/trace/reader/entities/AttributeValueConverter.java
@@ -1,12 +1,15 @@
 package org.hypertrace.trace.reader.entities;
 
 import io.reactivex.rxjava3.core.Maybe;
-import io.reactivex.rxjava3.core.Single;
 import org.hypertrace.core.attribute.service.v1.LiteralValue;
 import org.hypertrace.entity.data.service.v1.AttributeValue;
 import org.hypertrace.entity.data.service.v1.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class AttributeValueConverter {
+  private static final Logger LOG = LoggerFactory.getLogger(AttributeValueConverter.class);
+
   static Maybe<AttributeValue> convertToAttributeValue(LiteralValue literalValue) {
     switch (literalValue.getValueCase()) {
       case STRING_VALUE:
@@ -20,9 +23,8 @@ class AttributeValueConverter {
       case VALUE_NOT_SET:
         return Maybe.empty();
       default:
-        return Maybe.error(
-            new UnsupportedOperationException(
-                "Unexpected literal value case: " + literalValue.getValueCase()));
+        LOG.error("Unexpected literal value case: " + literalValue.getValueCase());
+        return Maybe.empty();
     }
   }
 
