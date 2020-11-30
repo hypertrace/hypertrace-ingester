53d1520246da3d138b1d9cd8db3a09e0da448f7c
Rishabh <rishabh@traceable.ai>
2020-11-30 20:23:57 +0530

    Add otel constants for container & k8s (#78)
    * Add otel constants for container & k8s

---
+++ span-normalizer/span-normalizer-constants/src/main/java/org/hypertrace/core/semantic/convention/constants/resource/OTelContainerSemanticConventions.java
@@ -0,0 +1,19 @@
+package org.hypertrace.core.semantic.convention.constants.resource;
+
+/**
+ * Open telemetry container semantic conventions
+ */
+public enum OTelContainerSemanticConventions {
+  CONTAINER_NAME("container.name"),
+  CONTAINER_ID("container.id");
+
+  private final String value;
+
+  OTelContainerSemanticConventions(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+}
---
+++ span-normalizer/span-normalizer-constants/src/main/java/org/hypertrace/core/semantic/convention/constants/resource/OTelK8sSemanticConventions.java
@@ -0,0 +1,21 @@
+package org.hypertrace.core.semantic.convention.constants.resource;
+
+/**
+ * Open telemetry Semantic conventions for kubernetes objects and metadata
+ */
+public enum  OTelK8sSemanticConventions {
+  K8s_CLUSTER_NAME("k8s.cluster.name"),
+  K8s_NAMESPACE_NAME("k8s.namespace.name"),
+  K8s_POD_UID("k8s.pod.uid"),
+  K8s_POD_NAME("k8s.pod.name");
+
+  private final String value;
+
+  OTelK8sSemanticConventions(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+}
