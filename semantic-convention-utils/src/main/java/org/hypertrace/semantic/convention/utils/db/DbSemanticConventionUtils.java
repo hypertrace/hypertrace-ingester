e2544136e1da6508501df2a4feb155f58c7d4e40
Rishabh Singh <rishabh@traceable.ai>
2020-12-14 14:04:52 +0530

    Remove unusued imports

--- semantic-convention-utils/src/main/java/org/hypertrace/semantic/convention/utils/db/DbSemanticConventionUtils.java
+++ semantic-convention-utils/src/main/java/org/hypertrace/semantic/convention/utils/db/DbSemanticConventionUtils.java
@@ -2,10 +2,6 @@ package org.hypertrace.semantic.convention.utils.db;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
