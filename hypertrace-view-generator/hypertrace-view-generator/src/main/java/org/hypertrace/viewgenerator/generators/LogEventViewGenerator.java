4edb43c640e81d73a0d515dbe98721c7224512ea
rish691 <rishabh@traceable.ai>
2021-04-14 21:54:54 +0530

    Added some tests

--- hypertrace-view-generator/hypertrace-view-generator/src/main/java/org/hypertrace/viewgenerator/generators/LogEventViewGenerator.java
+++ hypertrace-view-generator/hypertrace-view-generator/src/main/java/org/hypertrace/viewgenerator/generators/LogEventViewGenerator.java
@@ -14,15 +14,14 @@ import org.hypertrace.viewgenerator.api.LogEventView;
 public class LogEventViewGenerator implements JavaCodeBasedViewGenerator<LogEvents, LogEventView> {
 
   @Override
-  public List<LogEventView> process(LogEvents logEventRecords) {
-    return logEventRecords.getLogEvents().stream()
-        .filter(v -> !v.getAttributes().getAttributeMap().isEmpty())
+  public List<LogEventView> process(LogEvents logEvents) {
+    return logEvents.getLogEvents().stream()
         .map(
             logEventRecord ->
                 LogEventView.newBuilder()
                     .setSpanId(logEventRecord.getSpanId())
                     .setTraceId(logEventRecord.getTraceId())
-                    .setTimeStamp(logEventRecord.getTimestampNanos())
+                    .setTimestampNanos(logEventRecord.getTimestampNanos())
                     .setTenantId(logEventRecord.getTenantId())
                     .setAttributes(convertAttributes(logEventRecord.getAttributes()))
                     .build())
@@ -30,6 +29,11 @@ public class LogEventViewGenerator implements JavaCodeBasedViewGenerator<LogEven
   }
 
   private String convertAttributes(Attributes attributes) {
+    if (null == attributes
+        || null == attributes.getAttributeMap()
+        || attributes.getAttributeMap().isEmpty()) {
+      return null;
+    }
     Map<String, String> resultMap = new HashMap<>();
 
     for (Map.Entry<String, AttributeValue> entry : attributes.getAttributeMap().entrySet()) {
