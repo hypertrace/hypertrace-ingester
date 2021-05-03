fa49511d680d4cfb7effc5959d40300d759a7a56
SJ <shubham@traceable.ai>
2021-05-03 17:25:19 +0530

    added fqn resolver doc comments

--- hypertrace-trace-enricher/hypertrace-trace-enricher-impl/src/main/java/org/hypertrace/traceenricher/enrichment/enrichers/backend/FqnResolver.java
+++ hypertrace-trace-enricher/hypertrace-trace-enricher-impl/src/main/java/org/hypertrace/traceenricher/enrichment/enrichers/backend/FqnResolver.java
@@ -2,6 +2,7 @@ package org.hypertrace.traceenricher.enrichment.enrichers.backend;
 
 import org.hypertrace.core.datamodel.Event;
 
+// Resolves a FQN (service, backend) from host name and event properties
 public interface FqnResolver {
   String resolve(String hostname, Event event);
 }
