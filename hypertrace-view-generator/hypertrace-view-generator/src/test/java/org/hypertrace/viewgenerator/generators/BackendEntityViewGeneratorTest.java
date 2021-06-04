60bf3b80302752ad48a66cfaf41315eea624c9e2
Sunn-y-Arora <sunny.arora@razorpay.com>
2021-06-04 16:15:25 +0530

    build fix

--- hypertrace-view-generator/hypertrace-view-generator/src/test/java/org/hypertrace/viewgenerator/generators/BackendEntityViewGeneratorTest.java
+++ hypertrace-view-generator/hypertrace-view-generator/src/test/java/org/hypertrace/viewgenerator/generators/BackendEntityViewGeneratorTest.java
@@ -1,11 +1,5 @@
 package org.hypertrace.viewgenerator.generators;
 
-import org.apache.avro.file.DataFileReader;
-import org.apache.avro.specific.SpecificDatumReader;
-import org.hypertrace.core.datamodel.Event;
-import org.hypertrace.core.datamodel.StructuredTrace;
-import org.hypertrace.viewgenerator.api.BackendEntityView;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
@@ -14,32 +8,57 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.hypertrace.core.datamodel.Event;
+import org.hypertrace.core.datamodel.StructuredTrace;
+import org.hypertrace.viewgenerator.api.BackendEntityView;
+import org.junit.jupiter.api.Test;
 
 public class BackendEntityViewGeneratorTest {
 
-    @Test
-    public void testBackendEntityViewGenerator_HotrodTrace() throws IOException {
-        URL resource = Thread.currentThread().getContextClassLoader().getResource("StructuredTrace-Hotrod.avro");
-        SpecificDatumReader<StructuredTrace> datumReader = new SpecificDatumReader<>(StructuredTrace.getClassSchema());
-        DataFileReader<StructuredTrace> dfrStructuredTrace = new DataFileReader<>(new File(resource.getPath()), datumReader);
-        StructuredTrace trace = dfrStructuredTrace.next();
-        dfrStructuredTrace.close();
-        BackendEntityViewGenerator backendEntityViewGenerator = new BackendEntityViewGenerator();
-        List<BackendEntityView> backendEntityViews = backendEntityViewGenerator.process(trace);
-        List<Event> computedBackendEvents = getEventsWithBackendEntity(trace);
-        assertEntity(backendEntityViews,computedBackendEvents);
-    }
+  @Test
+  public void testBackendEntityViewGenerator_HotrodTrace() throws IOException {
+    URL resource =
+        Thread.currentThread().getContextClassLoader().getResource("StructuredTrace-Hotrod.avro");
+    SpecificDatumReader<StructuredTrace> datumReader =
+        new SpecificDatumReader<>(StructuredTrace.getClassSchema());
+    DataFileReader<StructuredTrace> dfrStructuredTrace =
+        new DataFileReader<>(new File(resource.getPath()), datumReader);
+    StructuredTrace trace = dfrStructuredTrace.next();
+    dfrStructuredTrace.close();
+    BackendEntityViewGenerator backendEntityViewGenerator = new BackendEntityViewGenerator();
+    List<BackendEntityView> backendEntityViews = backendEntityViewGenerator.process(trace);
+    List<Event> computedBackendEvents = getEventsWithBackendEntity(trace);
+    assertEntity(backendEntityViews, computedBackendEvents);
+  }
 
-    private List<Event> getEventsWithBackendEntity(StructuredTrace trace){
-        return trace.getEventList().stream()
-                .filter(event -> event.getEnrichedAttributes().getAttributeMap().containsKey("BACKEND_ENTITY_ID"))
-                .collect(Collectors.toCollection(ArrayList::new));
-    }
+  private List<Event> getEventsWithBackendEntity(StructuredTrace trace) {
+    return trace.getEventList().stream()
+        .filter(
+            event ->
+                event.getEnrichedAttributes().getAttributeMap().containsKey("BACKEND_ENTITY_ID"))
+        .collect(Collectors.toCollection(ArrayList::new));
+  }
 
-    private void assertEntity(List<BackendEntityView> backendViews,List<Event> backendEntity){
-        //asserting only for 1 element, as with sample trace, only 1 backend exists (redis)
-        assertEquals(backendViews.size(),backendEntity.size());
-        assertEquals(backendViews.get(0).getBackendId(),backendEntity.get(0).getEnrichedAttributes().getAttributeMap().get("BACKEND_ENTITY_ID").getValue());
-        assertEquals(backendViews.get(0).getBackendName(),backendEntity.get(0).getEnrichedAttributes().getAttributeMap().get("BACKEND_ENTITY_NAME").getValue());
-    }
+  private void assertEntity(List<BackendEntityView> backendViews, List<Event> backendEntity) {
+    // asserting only for 1 element, as with sample trace, only 1 backend exists (redis)
+    assertEquals(backendViews.size(), backendEntity.size());
+    assertEquals(
+        backendViews.get(0).getBackendId(),
+        backendEntity
+            .get(0)
+            .getEnrichedAttributes()
+            .getAttributeMap()
+            .get("BACKEND_ENTITY_ID")
+            .getValue());
+    assertEquals(
+        backendViews.get(0).getBackendName(),
+        backendEntity
+            .get(0)
+            .getEnrichedAttributes()
+            .getAttributeMap()
+            .get("BACKEND_ENTITY_NAME")
+            .getValue());
+  }
 }
