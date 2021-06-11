fff00960e89f826d672a1c52d9db7441c01f1768
Sunn-y-Arora <sunny.arora@razorpay.com>
2021-06-11 14:55:25 +0530

    gson serializer/desiralizer for ByteBuffer

---
+++ hypertrace-view-generator/hypertrace-view-generator/src/test/java/org/hypertrace/viewgenerator/generators/utils/ByteBufferTypeAdapter.java
@@ -0,0 +1,22 @@
+package org.hypertrace.viewgenerator.generators.utils;
+
+import com.google.gson.*;
+import org.apache.commons.codec.binary.Base64;
+
+
+import java.lang.reflect.Type;
+import java.nio.ByteBuffer;
+
+
+
+public class ByteBufferTypeAdapter implements JsonDeserializer<ByteBuffer>, JsonSerializer<ByteBuffer> {
+
+    public ByteBuffer deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context){
+        return ByteBuffer.wrap(Base64.decodeBase64(jsonElement.getAsString()));
+    }
+
+    @Override
+    public JsonElement serialize(ByteBuffer src, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(Base64.encodeBase64String(src.array()));
+    }
+}
