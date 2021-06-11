5c7dc3bc3e997dfaac59e050fde6ff7b49a1fcc1
Sunn-y-Arora <sunny.arora@razorpay.com>
2021-06-11 15:00:00 +0530

    spotless apply

--- hypertrace-view-generator/hypertrace-view-generator/src/test/java/org/hypertrace/viewgenerator/generators/utils/ByteBufferTypeAdapter.java
+++ hypertrace-view-generator/hypertrace-view-generator/src/test/java/org/hypertrace/viewgenerator/generators/utils/ByteBufferTypeAdapter.java
@@ -1,22 +1,20 @@
 package org.hypertrace.viewgenerator.generators.utils;
 
 import com.google.gson.*;
-import org.apache.commons.codec.binary.Base64;
-
-
 import java.lang.reflect.Type;
 import java.nio.ByteBuffer;
+import org.apache.commons.codec.binary.Base64;
 
+public class ByteBufferTypeAdapter
+    implements JsonDeserializer<ByteBuffer>, JsonSerializer<ByteBuffer> {
 
+  public ByteBuffer deserialize(
+      JsonElement jsonElement, Type type, JsonDeserializationContext context) {
+    return ByteBuffer.wrap(Base64.decodeBase64(jsonElement.getAsString()));
+  }
 
-public class ByteBufferTypeAdapter implements JsonDeserializer<ByteBuffer>, JsonSerializer<ByteBuffer> {
-
-    public ByteBuffer deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context){
-        return ByteBuffer.wrap(Base64.decodeBase64(jsonElement.getAsString()));
-    }
-
-    @Override
-    public JsonElement serialize(ByteBuffer src, Type typeOfSrc, JsonSerializationContext context) {
-        return new JsonPrimitive(Base64.encodeBase64String(src.array()));
-    }
+  @Override
+  public JsonElement serialize(ByteBuffer src, Type typeOfSrc, JsonSerializationContext context) {
+    return new JsonPrimitive(Base64.encodeBase64String(src.array()));
+  }
 }
--- hypertrace-view-generator/hypertrace-view-generator/src/test/java/org/hypertrace/viewgenerator/generators/utils/TestUtilities.java
+++ hypertrace-view-generator/hypertrace-view-generator/src/test/java/org/hypertrace/viewgenerator/generators/utils/TestUtilities.java
@@ -1,30 +1,24 @@
 package org.hypertrace.viewgenerator.generators.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.net.URL;
 import java.nio.ByteBuffer;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import org.hypertrace.core.datamodel.StructuredTrace;
 
 public class TestUtilities {
 
-  private static Gson gson = new GsonBuilder().serializeNulls().registerTypeHierarchyAdapter(ByteBuffer.class,new ByteBufferTypeAdapter()).create();
-
+  private static Gson gson =
+      new GsonBuilder()
+          .serializeNulls()
+          .registerTypeHierarchyAdapter(ByteBuffer.class, new ByteBufferTypeAdapter())
+          .create();
 
   public static StructuredTrace getSampleHotRodTrace() throws FileNotFoundException {
     URL resource =
-            Thread.currentThread().getContextClassLoader().getResource("StructuredTrace-Hotrod.json");
-    return gson.fromJson(new FileReader(resource.getPath()),StructuredTrace.class);
+        Thread.currentThread().getContextClassLoader().getResource("StructuredTrace-Hotrod.json");
+    return gson.fromJson(new FileReader(resource.getPath()), StructuredTrace.class);
   }
-
-//  @Test
-//  void testGson() throws FileNotFoundException {
-//    Gson gson = new GsonBuilder().registerTypeHierarchyAdapter(ByteBuffer.class,new ByteBufferTypeAdapter()).create();
-//    StructuredTrace trace = gson.fromJson(new FileReader(jsonResource.getPath()),StructuredTrace.class);
-//    System.out.println("fadf");
-//  }
 }
