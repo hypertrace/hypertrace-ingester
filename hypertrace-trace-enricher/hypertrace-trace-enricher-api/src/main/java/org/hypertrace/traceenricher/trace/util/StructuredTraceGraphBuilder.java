9ad319a9c30859d6357b67202e0c0f7121aa4c7c
Rishabh Singh <rishabh@traceable.ai>
2021-06-14 19:49:40 +0530

    enhancement: Update StructuredGraph rebuilding logic (#205)

--- hypertrace-trace-enricher/hypertrace-trace-enricher-api/src/main/java/org/hypertrace/traceenricher/trace/util/StructuredTraceGraphBuilder.java
+++ hypertrace-trace-enricher/hypertrace-trace-enricher-api/src/main/java/org/hypertrace/traceenricher/trace/util/StructuredTraceGraphBuilder.java
@@ -2,7 +2,6 @@ package org.hypertrace.traceenricher.trace.util;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.concurrent.TimeUnit;
 import org.hypertrace.core.datamodel.StructuredTrace;
 import org.hypertrace.core.datamodel.shared.StructuredTraceGraph;
 import org.slf4j.Logger;
@@ -11,22 +10,44 @@ import org.slf4j.LoggerFactory;
 public class StructuredTraceGraphBuilder {
   private static final Logger LOG = LoggerFactory.getLogger(StructuredTraceGraphBuilder.class);
 
-  private static ThreadLocal<StructuredTraceGraph> cachedGraph = new ThreadLocal<>();
-  private static ThreadLocal<StructuredTrace> cachedTrace = new ThreadLocal<>();
+  private static final ThreadLocal<StructuredTraceGraph> cachedGraphThreadLocal =
+      new ThreadLocal<>();
+  private static final ThreadLocal<StructuredTrace> cachedTraceThreadLocal = new ThreadLocal<>();
 
   public static StructuredTraceGraph buildGraph(StructuredTrace trace) {
-    if (!GraphBuilderUtil.isSameStructuredTrace(cachedTrace.get(), trace)) {
+    StructuredTrace cachedTrace = cachedTraceThreadLocal.get();
+    boolean shouldRebuildTraceEventsGraph =
+        GraphBuilderUtil.isTraceEventsChanged(cachedTrace, trace);
+    boolean shouldRebuildTraceEntitiesGraph =
+        GraphBuilderUtil.isTraceEntitiesChanged(cachedTrace, trace);
+
+    if (GraphBuilderUtil.isDifferentTrace(cachedTrace, trace)
+        || (shouldRebuildTraceEventsGraph && shouldRebuildTraceEntitiesGraph)) {
       Instant start = Instant.now();
       StructuredTraceGraph graph = StructuredTraceGraph.createGraph(trace);
       LOG.debug(
-          "Time taken in building StructuredTraceGraph:{} for tenantId:{}",
+          "Time taken in building StructuredTraceGraph, duration_millis:{} for tenantId:{}",
           Duration.between(start, Instant.now()).toMillis(),
-          TimeUnit.MILLISECONDS,
           trace.getCustomerId());
-      cachedTrace.set(StructuredTrace.newBuilder(trace).build());
-      cachedGraph.set(graph);
+      cachedTraceThreadLocal.set(StructuredTrace.newBuilder(trace).build());
+      cachedGraphThreadLocal.set(graph);
       return graph;
     }
-    return cachedGraph.get();
+
+    if (shouldRebuildTraceEventsGraph || shouldRebuildTraceEntitiesGraph) {
+      Instant start = Instant.now();
+      StructuredTraceGraph graph =
+          shouldRebuildTraceEventsGraph
+              ? StructuredTraceGraph.reCreateTraceEventsGraph(trace)
+              : StructuredTraceGraph.reCreateTraceEntitiesGraph(trace);
+      LOG.debug(
+          "Time taken in building TraceEventsGraph, duration_millis:{} for tenantId:{}",
+          Duration.between(start, Instant.now()).toMillis(),
+          trace.getCustomerId());
+      cachedTraceThreadLocal.set(StructuredTrace.newBuilder(trace).build());
+      cachedGraphThreadLocal.set(graph);
+      return graph;
+    }
+    return cachedGraphThreadLocal.get();
   }
 }
