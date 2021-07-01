623d8d6ecb78a62b3fa1fe19af5b32bdb914b78c
Sarthak Singhal <sarthaksinghal@Sarthaks-MacBook-Pro.local>
2021-07-01 10:09:39 +0530

    updated servie framework

--- hypertrace-trace-enricher/hypertrace-trace-enricher-impl/build.gradle.kts
+++ hypertrace-trace-enricher/hypertrace-trace-enricher-impl/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
 
   implementation("org.hypertrace.core.datamodel:data-model:0.1.16")
   implementation("org.hypertrace.entity.service:entity-service-client:0.6.4")
-  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.23")
+  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.26")
   implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.4.0")
   implementation("org.hypertrace.config.service:spaces-config-service-api:0.1.0")
   implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.4.0")
--- span-normalizer/span-normalizer/build.gradle.kts
+++ span-normalizer/span-normalizer/build.gradle.kts
@@ -35,8 +35,8 @@ dependencies {
   implementation(project(":semantic-convention-utils"))
 
   implementation("org.hypertrace.core.datamodel:data-model:0.1.16")
-  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.23")
-  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.23")
+  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.26")
+  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.26")
   implementation("org.hypertrace.core.kafkastreams.framework:kafka-streams-framework:0.1.21")
 
   // Required for the GRPC clients.
@@ -64,7 +64,7 @@ dependencies {
   runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
-  testImplementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.23")
+  testImplementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.26")
   testImplementation("org.junit-pioneer:junit-pioneer:1.3.8")
   testImplementation("org.apache.kafka:kafka-streams-test-utils:6.0.1-ccs")
 }
