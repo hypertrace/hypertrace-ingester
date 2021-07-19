3824442a5faa6258dbb749104c0c83ea9977f81a
Aaron Steinfeld <aaron@traceable.ai>
2021-07-19 10:13:58 -0400

    chore: update vulnerable deps

--- hypertrace-view-generator/hypertrace-view-generator-api/build.gradle.kts
+++ hypertrace-view-generator/hypertrace-view-generator-api/build.gradle.kts
@@ -13,4 +13,9 @@ sourceSets {
 
 dependencies {
   api("org.apache.avro:avro:1.10.2")
+  constraints {
+    api("org.apache.commons:commons-compress:1.21") {
+      because("Multiple vulnerabilities in avro-declared version")
+    }
+  }
 }
