c5ffb251c9b1865196a478157a5f79e50cff98cf
Aaron Steinfeld <aaron@traceable.ai>
2021-07-21 09:00:25 -0400

    chore: update entity libs

--- hypertrace-trace-enricher/hypertrace-trace-enricher-impl/build.gradle.kts
+++ hypertrace-trace-enricher/hypertrace-trace-enricher-impl/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
   implementation(project(":hypertrace-trace-enricher:trace-reader"))
 
   implementation("org.hypertrace.core.datamodel:data-model:0.1.17")
-  implementation("org.hypertrace.entity.service:entity-service-client:0.6.8")
+  implementation("org.hypertrace.entity.service:entity-service-client:0.6.10")
   implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.26")
   implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.5.2")
   implementation("org.hypertrace.config.service:spaces-config-service-api:0.1.0")
