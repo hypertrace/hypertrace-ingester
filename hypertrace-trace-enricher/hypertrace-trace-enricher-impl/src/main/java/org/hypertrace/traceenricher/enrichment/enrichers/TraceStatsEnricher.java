5cec10b93fe89a41908ef49985a671240ad03799
Denys Sinyakov <denys@traceable.ai>
2021-08-27 11:04:35 -0700

    moved tests to enricher

---
+++ hypertrace-trace-enricher/hypertrace-trace-enricher-impl/src/main/java/org/hypertrace/traceenricher/enrichment/enrichers/TraceStatsEnricher.java
@@ -0,0 +1,13 @@
+package org.hypertrace.traceenricher.enrichment.enrichers;
+
+import org.hypertrace.core.datamodel.StructuredTrace;
+import org.hypertrace.traceenricher.enrichment.AbstractTraceEnricher;
+import org.hypertrace.traceenricher.trace.util.ApiTraceGraph;
+import org.hypertrace.traceenricher.trace.util.ApiTraceGraphBuilder;
+
+public class TraceStatsEnricher extends AbstractTraceEnricher {
+
+  @Override
+  public void enrichTrace(StructuredTrace trace) {
+  }
+}
