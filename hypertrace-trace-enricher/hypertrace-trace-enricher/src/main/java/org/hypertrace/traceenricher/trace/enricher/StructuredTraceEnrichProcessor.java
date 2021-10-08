8e726054abbd912b0fdf6a98756c23c66e7a5186
Avinash <52672355+avinashkolluru@users.noreply.github.com>
2021-10-08 10:57:56 +0530

    Added support for async cache loading in EntityCache and EdsCacheClient (#267)
    * Added support for async cache loading in EntityCache and EdsCacheClient
    
    * Added refreshAfterWrite for backend cache

--- hypertrace-trace-enricher/hypertrace-trace-enricher/src/main/java/org/hypertrace/traceenricher/trace/enricher/StructuredTraceEnrichProcessor.java
+++ hypertrace-trace-enricher/hypertrace-trace-enricher/src/main/java/org/hypertrace/traceenricher/trace/enricher/StructuredTraceEnrichProcessor.java
@@ -1,14 +1,18 @@
 package org.hypertrace.traceenricher.trace.enricher;
 
+import static org.hypertrace.traceenricher.trace.enricher.StructuredTraceEnricherConstants.CACHE_LOADER_THREAD_POOL_SIZE;
 import static org.hypertrace.traceenricher.trace.enricher.StructuredTraceEnricherConstants.ENRICHER_CLIENTS_CONFIG_KEY;
 import static org.hypertrace.traceenricher.trace.enricher.StructuredTraceEnricherConstants.ENRICHER_CONFIG_TEMPLATE;
 import static org.hypertrace.traceenricher.trace.enricher.StructuredTraceEnricherConstants.ENRICHER_NAMES_CONFIG_KEY;
 import static org.hypertrace.traceenricher.trace.enricher.StructuredTraceEnricherConstants.STRUCTURED_TRACES_ENRICHMENT_JOB_CONFIG_KEY;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.typesafe.config.Config;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Transformer;
 import org.apache.kafka.streams.processor.ProcessorContext;
@@ -28,10 +32,18 @@ public class StructuredTraceEnrichProcessor
     if (processor == null) {
       synchronized (StructuredTraceEnrichProcessor.class) {
         if (processor == null) {
-          Map<String, Config> enricherConfigs = getEnricherConfigs(context.appConfigs());
+          Config jobConfig =
+              (Config) context.appConfigs().get(STRUCTURED_TRACES_ENRICHMENT_JOB_CONFIG_KEY);
+          Map<String, Config> enricherConfigs = getEnricherConfigs(jobConfig);
           EnrichmentRegistry enrichmentRegistry = new EnrichmentRegistry();
           enrichmentRegistry.registerEnrichers(enricherConfigs);
-          clientRegistry = new DefaultClientRegistry(this.getClientsConfig(context.appConfigs()));
+          Config clientsConfig = this.getClientsConfig(jobConfig);
+          clientRegistry =
+              new DefaultClientRegistry(
+                  clientsConfig,
+                  Executors.newFixedThreadPool(
+                      getCacheLoaderExecutorThreadsPoolSize(clientsConfig),
+                      this.buildThreadFactory()));
           processor =
               new EnrichmentProcessor(
                   enrichmentRegistry.getOrderedRegisteredEnrichers(), clientRegistry);
@@ -51,8 +63,7 @@ public class StructuredTraceEnrichProcessor
     // clientRegistry.shutdown(); TODO restore cleanup once shared static instance removed
   }
 
-  private Map<String, Config> getEnricherConfigs(Map<String, Object> properties) {
-    Config jobConfig = (Config) properties.get(STRUCTURED_TRACES_ENRICHMENT_JOB_CONFIG_KEY);
+  private Map<String, Config> getEnricherConfigs(Config jobConfig) {
     List<String> enrichers = jobConfig.getStringList(ENRICHER_NAMES_CONFIG_KEY);
     Map<String, Config> enricherConfigs = new LinkedHashMap<>();
     for (String enricher : enrichers) {
@@ -62,12 +73,24 @@ public class StructuredTraceEnrichProcessor
     return enricherConfigs;
   }
 
-  private Config getClientsConfig(Map<String, Object> properties) {
-    Config jobConfig = (Config) properties.get(STRUCTURED_TRACES_ENRICHMENT_JOB_CONFIG_KEY);
+  private Config getClientsConfig(Config jobConfig) {
     return jobConfig.getConfig(ENRICHER_CLIENTS_CONFIG_KEY);
+  }
+
+  private int getCacheLoaderExecutorThreadsPoolSize(Config clientsConfig) {
+    return clientsConfig.hasPath(CACHE_LOADER_THREAD_POOL_SIZE)
+        ? clientsConfig.getInt(CACHE_LOADER_THREAD_POOL_SIZE)
+        : 3;
   }
 
   private String getEnricherConfigPath(String enricher) {
     return String.format(ENRICHER_CONFIG_TEMPLATE, enricher);
+  }
+
+  private ThreadFactory buildThreadFactory() {
+    return new ThreadFactoryBuilder()
+        .setDaemon(true)
+        .setNameFormat("async-cache-loader-%d")
+        .build();
   }
 }
