8b73714d5c81c99bf267e735b19e7e082f811421
Ronak <ronak@traceable.ai>
2021-11-23 21:32:01 +0530

    address comments of wrapping around if debuged enabled for log

--- span-normalizer/span-normalizer/src/main/java/org/hypertrace/core/spannormalizer/jaeger/SpanFilter.java
+++ span-normalizer/span-normalizer/src/main/java/org/hypertrace/core/spannormalizer/jaeger/SpanFilter.java
@@ -130,7 +130,7 @@ public class SpanFilter {
     }
 
     if (anySpanDropFiltersMatch(spanDropFilters, tags, processTags)) {
-      if (DROPPED_SPANS_RATE_LIMITER.tryAcquire()) {
+      if (LOG.isDebugEnabled() && DROPPED_SPANS_RATE_LIMITER.tryAcquire()) {
         LOG.debug("Dropping span: [{}] with drop filters: [{}]", span, spanDropFilters.toString());
       }
       return true;
