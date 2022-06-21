7954357ff8dc207e8bf69e15c7f7cc00bd279852
Aaron Steinfeld <aaron@traceable.ai>
2022-06-21 12:48:17 -0400

    chore: use release versison

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
   id("org.hypertrace.repository-plugin") version "0.4.0"
   id("org.hypertrace.ci-utils-plugin") version "0.3.0"
-  id("org.hypertrace.docker-java-application-plugin") version "0.9.1-SNAPSHOT" apply false
-  id("org.hypertrace.docker-publish-plugin") version "0.9.1-SNAPSHOT" apply false
+  id("org.hypertrace.docker-java-application-plugin") version "0.9.3" apply false
+  id("org.hypertrace.docker-publish-plugin") version "0.9.3" apply false
   id("org.hypertrace.jacoco-report-plugin") version "0.2.0" apply false
   id("org.hypertrace.publish-plugin") version "1.0.2" apply false
   id("org.hypertrace.avro-plugin") version "0.3.1" apply false
