1690d8d49baed0439753bb8b67a240f9c50dc580
surajpuvvada <suraj@traceable.ai>
2022-08-24 15:00:29 -0700

    update jetty version

--- hypertrace-metrics-exporter/hypertrace-metrics-exporter/build.gradle.kts
+++ hypertrace-metrics-exporter/hypertrace-metrics-exporter/build.gradle.kts
@@ -26,8 +26,8 @@ tasks.test {
 
 dependencies {
   // common and framework
-  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.33")
-  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.33")
+  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.43")
+  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.43")
 
   // open telemetry
   implementation("io.opentelemetry:opentelemetry-sdk-metrics:1.7.0-alpah")
