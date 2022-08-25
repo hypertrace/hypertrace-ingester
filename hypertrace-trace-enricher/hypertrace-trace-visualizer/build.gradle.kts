430adfcefa183d382c5ecfb451e9ef8c6608d8ad
GurtejSohi <gurtejsohi25@gmail.com>
2022-08-25 17:32:22 +0530

    chore: upgrade data-model and avro version

--- hypertrace-trace-enricher/hypertrace-trace-visualizer/build.gradle.kts
+++ hypertrace-trace-enricher/hypertrace-trace-visualizer/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-  implementation("org.hypertrace.core.datamodel:data-model:0.1.22")
+  implementation("org.hypertrace.core.datamodel:data-model:0.1.23")
 
   implementation("org.json:json:20210307")
   implementation("org.apache.commons:commons-lang3:3.12.0")
--- raw-spans-grouper/raw-spans-grouper/build.gradle.kts
+++ raw-spans-grouper/raw-spans-grouper/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
         because("https://snyk.io/vuln/SNYK-JAVA-ORGGLASSFISHJERSEYCORE-1255637")
     }
     implementation(project(":span-normalizer:span-normalizer-api"))
-    implementation("org.hypertrace.core.datamodel:data-model:0.1.22")
+    implementation("org.hypertrace.core.datamodel:data-model:0.1.23")
     implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.43")
     implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.43")
 
