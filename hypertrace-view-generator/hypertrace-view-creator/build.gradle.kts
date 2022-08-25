62d8d41ae5850201512c4a7279b0521dcd4f8139
GurtejSohi <gurtejsohi25@gmail.com>
2022-08-25 18:36:49 +0530

    upgrade view-creator-framework lib

--- hypertrace-view-generator/hypertrace-view-creator/build.gradle.kts
+++ hypertrace-view-generator/hypertrace-view-creator/build.gradle.kts
@@ -17,7 +17,7 @@ tasks.test {
 
 dependencies {
   implementation(project(":hypertrace-view-generator:hypertrace-view-generator-api"))
-  implementation("org.hypertrace.core.viewcreator:view-creator-framework:0.3.10")
+  implementation("org.hypertrace.core.viewcreator:view-creator-framework:0.4.5")
   constraints {
     // to have calcite libs on the same version
     implementation("org.apache.calcite:calcite-babel:1.26.0") {
