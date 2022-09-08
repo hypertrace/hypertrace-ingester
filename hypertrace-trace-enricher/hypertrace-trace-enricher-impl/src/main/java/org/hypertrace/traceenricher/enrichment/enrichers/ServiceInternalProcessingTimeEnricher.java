dc0bc4a0f0826b5859a98983f5cca0f1132759ed
Prashant Pandey <prashant.pandey@razorpay.com>
2022-09-08 15:02:36 +0530

    Handle exception

--- hypertrace-trace-enricher/hypertrace-trace-enricher-impl/src/main/java/org/hypertrace/traceenricher/enrichment/enrichers/ServiceInternalProcessingTimeEnricher.java
+++ hypertrace-trace-enricher/hypertrace-trace-enricher-impl/src/main/java/org/hypertrace/traceenricher/enrichment/enrichers/ServiceInternalProcessingTimeEnricher.java
@@ -21,64 +21,69 @@ public class ServiceInternalProcessingTimeEnricher extends AbstractTraceEnricher
       LoggerFactory.getLogger(ServiceInternalProcessingTimeEnricher.class);
 
   public void enrichTrace(StructuredTrace trace) {
-    ApiTraceGraph apiTraceGraph = ApiTraceGraphBuilder.buildGraph(trace);
-    List<ApiNode<Event>> apiNodeList = apiTraceGraph.getApiNodeList();
-    for (ApiNode<Event> apiNode : apiNodeList) {
-      List<Event> exitApiBoundaryEvents = apiNode.getExitApiBoundaryEvents();
-      List<ApiNodeEventEdge> edges = apiTraceGraph.getOutboundEdgesForApiNode(apiNode);
-      int edgeDurationSum = 0;
-      // Note: this logic of summing the duration of each child span does not work if children spans
-      // were
-      // concurrent to one-another. In that case, the parent span waits only for
-      // max(duration_child_1,
-      // duration_child2,...,duration_child_n) and not duration_child1 + duration_child_2 +
-      // duration_child_3
-      // Works for:
-      // |------------------PARENT-------------------|
-      //  |---C1---|
-      //              |---C2---|
-      //                          |---C3---|
-      // Doesn't work for:
-      // |------------------PARENT-------------------|
-      //  |---C1---|
-      //    |---C2---|
-      //      |---C3---|
-      for (var edge : edges) {
-        edgeDurationSum += getApiNodeEventEdgeDuration(edge);
-      }
-      // now sum up http or https backends
-      double httpExitCallsSum =
-          exitApiBoundaryEvents.stream()
-              .filter(
-                  event -> {
-                    Map<String, AttributeValue> enrichedAttributes =
-                        event.getEnrichedAttributes().getAttributeMap();
-                    return enrichedAttributes.containsKey("BACKEND_PROTOCOL")
-                        && enrichedAttributes.get("BACKEND_PROTOCOL").getValue().contains("HTTP");
-                  })
-              .mapToDouble(event -> event.getMetrics().getMetricMap().get("Duration").getValue())
-              .sum();
-      Optional<Event> entryApiBoundaryEventMaybe = apiNode.getEntryApiBoundaryEvent();
-      if (entryApiBoundaryEventMaybe.isPresent()) {
-        var entryApiBoundaryEvent = entryApiBoundaryEventMaybe.get();
-        var entryApiBoundaryEventDuration = getEventDuration(entryApiBoundaryEvent);
-        try {
-          entryApiBoundaryEvent
-              .getAttributes()
-              .getAttributeMap()
-              .put(
-                  EnrichedSpanConstants.INTERNAL_SVC_LATENCY,
-                  AttributeValueCreator.create(
-                      String.valueOf(
-                          entryApiBoundaryEventDuration - edgeDurationSum - httpExitCallsSum)));
-        } catch (NullPointerException e) {
-          LOG.error(
-              "NPE while calculating service internal time. entryApiBoundaryEventDuration {}, edgeDurationSum {}",
-              entryApiBoundaryEventDuration,
-              edgeDurationSum,
-              e);
+    try {
+      ApiTraceGraph apiTraceGraph = ApiTraceGraphBuilder.buildGraph(trace);
+      List<ApiNode<Event>> apiNodeList = apiTraceGraph.getApiNodeList();
+      for (ApiNode<Event> apiNode : apiNodeList) {
+        List<Event> exitApiBoundaryEvents = apiNode.getExitApiBoundaryEvents();
+        List<ApiNodeEventEdge> edges = apiTraceGraph.getOutboundEdgesForApiNode(apiNode);
+        int edgeDurationSum = 0;
+        // Note: this logic of summing the duration of each child span does not work if children spans
+        // were
+        // concurrent to one-another. In that case, the parent span waits only for
+        // max(duration_child_1,
+        // duration_child2,...,duration_child_n) and not duration_child1 + duration_child_2 +
+        // duration_child_3
+        // Works for:
+        // |------------------PARENT-------------------|
+        //  |---C1---|
+        //              |---C2---|
+        //                          |---C3---|
+        // Doesn't work for:
+        // |------------------PARENT-------------------|
+        //  |---C1---|
+        //    |---C2---|
+        //      |---C3---|
+        for (var edge : edges) {
+          edgeDurationSum += getApiNodeEventEdgeDuration(edge);
+        }
+        // now sum up http or https backends
+        double httpExitCallsSum =
+            exitApiBoundaryEvents.stream()
+                .filter(
+                    event -> {
+                      Map<String, AttributeValue> enrichedAttributes =
+                          event.getEnrichedAttributes().getAttributeMap();
+                      return enrichedAttributes.containsKey("BACKEND_PROTOCOL")
+                          && enrichedAttributes.get("BACKEND_PROTOCOL").getValue().contains("HTTP");
+                    })
+                .mapToDouble(event -> event.getMetrics().getMetricMap().get("Duration").getValue())
+                .sum();
+        Optional<Event> entryApiBoundaryEventMaybe = apiNode.getEntryApiBoundaryEvent();
+        if (entryApiBoundaryEventMaybe.isPresent()) {
+          var entryApiBoundaryEvent = entryApiBoundaryEventMaybe.get();
+          var entryApiBoundaryEventDuration = getEventDuration(entryApiBoundaryEvent);
+          try {
+            entryApiBoundaryEvent
+                .getAttributes()
+                .getAttributeMap()
+                .put(
+                    EnrichedSpanConstants.INTERNAL_SVC_LATENCY,
+                    AttributeValueCreator.create(
+                        String.valueOf(
+                            entryApiBoundaryEventDuration - edgeDurationSum - httpExitCallsSum)));
+          } catch (NullPointerException e) {
+            LOG.error(
+                "NPE while calculating service internal time. entryApiBoundaryEventDuration {}, edgeDurationSum {}",
+                entryApiBoundaryEventDuration,
+                edgeDurationSum,
+                e);
+            throw e;
+          }
         }
       }
+    } catch (Exception e) {
+      LOG.error("Exception while calculating service internal time");
     }
   }
 
