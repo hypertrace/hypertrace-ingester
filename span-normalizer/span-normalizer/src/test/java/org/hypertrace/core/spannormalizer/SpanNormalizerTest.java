7805d5369eac4d2192982316e90d472a01b6a22f
Ronak <ronak@traceable.ai>
2022-12-27 23:50:09 +0530

    spotless fix

--- span-normalizer/span-normalizer/src/test/java/org/hypertrace/core/spannormalizer/SpanNormalizerTest.java
+++ span-normalizer/span-normalizer/src/test/java/org/hypertrace/core/spannormalizer/SpanNormalizerTest.java
@@ -229,7 +229,8 @@ class SpanNormalizerTest {
     inputTopic.pipeInput(span5);
     assertTrue(outputTopic.isEmpty());
 
-    // pipe in one more span which does not match one of spanDropFilters (operation_name, and span.kind not
+    // pipe in one more span which does not match one of spanDropFilters (operation_name, and
+    // span.kind not
     // exists)
     Span span6 =
         Span.newBuilder()
@@ -255,7 +256,8 @@ class SpanNormalizerTest {
         HexUtils.getHex(ByteString.copyFrom("trace-6".getBytes()).toByteArray()),
         HexUtils.getHex(span6KV.key.getTraceId().array()));
 
-    // pipe in one more span which does not match one of spanDropFilters (operation_name, and span.kind not
+    // pipe in one more span which does not match one of spanDropFilters (operation_name, and
+    // span.kind not
     // exists)
     Span span7 =
         Span.newBuilder()
