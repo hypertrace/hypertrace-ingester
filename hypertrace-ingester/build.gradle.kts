9337e9ae531f929e5f733c67560da97e9c5ea6f9
Ronak <ronak@traceable.ai>
2023-02-22 17:21:57 +0530

    fixed spotless issue

--- hypertrace-ingester/build.gradle.kts
+++ hypertrace-ingester/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
   implementation("org.hypertrace.core.kafkastreams.framework:kafka-streams-framework:0.2.4")
   implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.49")
   implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.49")
-  
+
   implementation("org.hypertrace.core.datamodel:data-model:0.1.27")
   implementation("org.hypertrace.core.viewgenerator:view-generator-framework:0.4.10")
   implementation("org.apache.commons:commons-lang3:3.12.0")
