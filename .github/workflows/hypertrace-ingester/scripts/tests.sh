c3171ed9437db19fee8db38856957c1cf49ed46f
kishansairam9 <kishan.adapa@traceable.ai>
2023-10-13 20:58:37 +0530

    update

--- .github/workflows/hypertrace-ingester/scripts/tests.sh
+++ .github/workflows/hypertrace-ingester/scripts/tests.sh
@@ -39,7 +39,7 @@ echo ""
 curl -o /dev/null -s http://${FRONTEND_SERVICE_HOST}:8081  || { echo "Host $FRONTEND_SERVICE_HOST is down." ; exit 1; }
 
 # grouper depends on stream time advancement, hence we are sending another set of traces just to meet that condition
-sleep 60
+sleep 35
 curl -o /dev/null -s http://${FRONTEND_SERVICE_HOST}:8081  || { echo "Host $FRONTEND_SERVICE_HOST is down." ; exit 1; }
 
 echo "Retrieving the list of traces."
