1aea26b770838f15b8d8feb48df67548ce40d73f
Kishan Sairam Adapa <kishan.adapa@traceable.ai>
2023-11-22 21:17:56 +0530

    update gradle to 8 (#440)
    * update gradle to 8
    
    * update
    
    * update
    
    * remove grpc
    
    * spotless

--- hypertrace-trace-enricher/enriched-span-constants/build.gradle.kts
+++ hypertrace-trace-enricher/enriched-span-constants/build.gradle.kts
@@ -1,65 +1,29 @@
-import com.google.protobuf.gradle.generateProtoTasks
 import com.google.protobuf.gradle.id
-import com.google.protobuf.gradle.ofSourceSet
-import com.google.protobuf.gradle.plugins
-import com.google.protobuf.gradle.protobuf
-import com.google.protobuf.gradle.protoc
 
 plugins {
   `java-library`
   jacoco
   id("org.hypertrace.jacoco-report-plugin")
-  id("com.google.protobuf") version "0.8.15"
+  id("com.google.protobuf") version "0.9.2"
   id("org.hypertrace.publish-plugin")
 }
 
-val generateLocalGoGrpcFiles = false
-
 protobuf {
   protoc {
-    artifact = "com.google.protobuf:protoc:3.21.1"
+    artifact = "com.google.protobuf:protoc:3.21.12"
   }
-  plugins {
-    id("grpc_java") {
-      artifact = "io.grpc:protoc-gen-grpc-java:1.57.2"
-    }
+}
 
-    if (generateLocalGoGrpcFiles) {
-      id("grpc_go") {
-        path = "<go-path>/bin/protoc-gen-go"
-      }
-    }
-  }
-  generateProtoTasks {
-    ofSourceSet("main").forEach {
-      it.plugins {
-        // Apply the "grpc" plugin whose spec is defined above, without options.
-        id("grpc_java")
-
-        if (generateLocalGoGrpcFiles) {
-          id("grpc_go")
-        }
-      }
-      it.builtins {
-        java
-        if (generateLocalGoGrpcFiles) {
-          id("go")
-        }
-      }
+sourceSets {
+  main {
+    java {
+      srcDirs("build/generated/source/proto/main/java")
     }
   }
 }
 
 tasks.test {
   useJUnitPlatform()
-}
-
-sourceSets {
-  main {
-    java {
-      srcDirs("src/main/java", "build/generated/source/proto/main/java", "build/generated/source/proto/main/grpc_java")
-    }
-  }
 }
 
 dependencies {
