5e2cc482c72fa0d61bef50649957ffd6649bc2c6
kishansairam9 <kishan.adapa@traceable.ai>
2024-01-08 17:32:08 +0530

    remove java 11 compat override

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,6 @@ subprojects {
 
   pluginManager.withPlugin("java") {
     configure<JavaPluginExtension> {
-      sourceCompatibility = JavaVersion.VERSION_11
-      targetCompatibility = JavaVersion.VERSION_11
-
       apply(plugin = "org.hypertrace.code-style-plugin")
     }
   }
