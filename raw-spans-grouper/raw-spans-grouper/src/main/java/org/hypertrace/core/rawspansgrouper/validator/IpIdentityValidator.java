44f600d411bc82dc144ab1153003753a813012f2
Sanket Mundra <sanket.mundra@traceable.ai>
2024-03-11 23:00:50 +0530

    remove self correlation and default value

--- raw-spans-grouper/raw-spans-grouper/src/main/java/org/hypertrace/core/rawspansgrouper/validator/IpIdentityValidator.java
+++ raw-spans-grouper/raw-spans-grouper/src/main/java/org/hypertrace/core/rawspansgrouper/validator/IpIdentityValidator.java
@@ -4,12 +4,23 @@ import java.util.Objects;
 import org.hypertrace.core.spannormalizer.IpIdentity;
 
 public class IpIdentityValidator {
+  private static final String DEFAULT_ADDR_VALUE = "";
+  private static final String DEFAULT_PORT_VALUE = "0";
+
   public static boolean isValid(IpIdentity ipIdentity) {
     if (Objects.isNull(ipIdentity.getEnvironment()) || Objects.isNull(ipIdentity.getTenantId())) {
       return false;
     }
 
-    return Objects.nonNull(ipIdentity.getHostAddr())
-        || (Objects.nonNull(ipIdentity.getPeerAddr()) && Objects.nonNull(ipIdentity.getPeerPort()));
+    if (Objects.nonNull(ipIdentity.getHostAddr()) && Objects.nonNull(ipIdentity.getPeerAddr())) {
+      return !ipIdentity.getHostAddr().equals(ipIdentity.getPeerAddr());
+    }
+
+    return (Objects.nonNull(ipIdentity.getHostAddr())
+            && !DEFAULT_ADDR_VALUE.equals(ipIdentity.getHostAddr()))
+        || (Objects.nonNull(ipIdentity.getPeerAddr())
+            && !DEFAULT_ADDR_VALUE.equals(ipIdentity.getPeerAddr())
+            && Objects.nonNull(ipIdentity.getPeerPort())
+            && !DEFAULT_PORT_VALUE.equals(ipIdentity.getPeerPort()));
   }
 }
